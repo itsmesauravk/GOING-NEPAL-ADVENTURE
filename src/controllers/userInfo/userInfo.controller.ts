a50cf390594e6ae0a0db00bd7fb15bb8c3fdf399
Saurav Karki <sauravkarki10.12@gmail.com>
2024-12-21 20:46:17 +0545

    udpated get user and delete user

--- src/controllers/userInfo/userInfo.controller.ts
+++ src/controllers/userInfo/userInfo.controller.ts
@@ -3,15 +3,35 @@ import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
 
 // Get all user details
-const getUserDetails = async (req: Request, res: Response) => {
+const getUserDetails = async (req: Request, res: Response): Promise<void> => {
   try {
-    const userDetails = await UserDetails.find()
+    const { search, sort } = req.query
+    let queryObject = {}
+
+    if (search) {
+      queryObject = { userName: { $regex: search, $options: "i" } as any }
+    }
+
+    let users = UserDetails.find(queryObject)
+
+    if (sort && typeof sort === "string") {
+      users = users.sort(sort)
+    }
+
+    let limit = Number(req.query.limit) || 20
+    let page = Number(req.query.page) || 1
+    let skip = (page - 1) * limit
+
+    users = users.skip(skip).limit(limit)
+
+    const userDetails = await users
 
     if (userDetails.length === 0) {
-      return res.status(StatusCodes.NOT_FOUND).json({
+      res.status(StatusCodes.NOT_FOUND).json({
         success: false,
         message: "No User Details Found",
       })
+      return
     }
 
     res.status(StatusCodes.OK).json({
@@ -26,4 +46,46 @@ const getUserDetails = async (req: Request, res: Response) => {
   }
 }
 
-export { getUserDetails }
+// delete user info
+const deleteUserDetails = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params
+    if (!id) {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: "Data is Invalid",
+        error: "Invalid data",
+      })
+      return
+    }
+
+    const userDetails = await UserDetails.findByIdAndDelete(id)
+    if (!userDetails) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: "Client Details not found",
+        error: `Client Details not found with id: ${id}`,
+      })
+      return
+    }
+
+    res
+      .status(StatusCodes.OK)
+      .json({ success: true, message: "Client Details deleted" })
+  } catch (error) {
+    let errorMessage = "Internal Server Error"
+    if (error instanceof Error) {
+      errorMessage = error.message
+    }
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Internal Server Error",
+      error: errorMessage,
+    })
+  }
+}
+
+export { getUserDetails, deleteUserDetails }
--- src/routes/userDetails.routes.ts
+++ src/routes/userDetails.routes.ts
@@ -1,10 +1,15 @@
 import express from "express"
 const router = express.Router()
 
-import { getUserDetails } from "../controllers/userInfo/userInfo.controller.js"
+import {
+  deleteUserDetails,
+  getUserDetails,
+} from "../controllers/userInfo/userInfo.controller.js"
 
 router.get("/get", async (req, res) => {
   await getUserDetails(req, res)
 })
+
+router.delete("/delete/:id", deleteUserDetails)
 
 export default router
