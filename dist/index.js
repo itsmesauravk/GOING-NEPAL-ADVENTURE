d94fcc8b04427520d6e6c5725dc946b898475328
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-07 23:44:31 +0545

    updated

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
-// import app from "./app.js"
-// //database
-// import connectDB from "./database/connectDB.js"
-// const PORT = process.env.PORT || 4000
-// connectDB()
-// app.listen(PORT, () => {
-//   console.log(`Server 📡 is running on port ${PORT}`)
-// })
 import app from "./app.js";
+//database
 import connectDB from "./database/connectDB.js";
-// Connect to the database (ensure it does not block execution)
+const PORT = process.env.PORT || 4000;
 connectDB();
-// Export the app for Vercel (DO NOT USE app.listen)
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server 📡 is running on port ${PORT}`);
+});
+// import app from "./app.js"
+// import connectDB from "./database/connectDB.js"
+// // Connect to the database (ensure it does not block execution)
+// connectDB()
+// // Export the app for Vercel (DO NOT USE app.listen)
+// export default app
